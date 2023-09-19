476aabcde67e2805784375864f2809bce462bbc2
Hertz-7 <mohammadaosaf33@gmail.com>
2023-09-19 13:08:55 +0500

    added flask wapper for calculator

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+from flask import Flask
+from flask import request
 
-def Multiply( num1, num2 ): 
-    answer = num1 * num2
-    return answer
+app = Flask(__name__)
 
+@app.route('/calc/mul/<int:a>/<int:b>', methods=['GET'])
+def Multiply( a, b ): 
+    
+    return str(a*b)
+
+@app.route('/calc/add/<int:a>/<int:b>', methods=['GET'])
 def add(a,b):
-    return a+b
+    return str(a+b)
 
+@app.route('/calc/sub/<int:a>/<int:b>', methods=['GET'])
 def sub(a,b):
-    return a-b
+    return str(a-b)
 
+if __name__ == '__main__':
+    app.run(debug=True)
